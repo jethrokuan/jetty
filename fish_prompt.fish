1ab1e9b59a4e2403e6c263eb9fbfd0887c9ea0ef
Jethro Kuan <jethro@tinkertanker.com>
2016-03-04 16:46:43 +0800

    Small Update

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,11 +1,13 @@
 function fish_prompt
   set -l BG "#D95B43"
+  set -l green "#7FFA0C"
+  set -l yellow "#C3FA0C"
   set -l cwd (prompt_pwd)
   set -l dir (dirname $cwd)
   set -l base (basename $cwd)
 
+  prompt_git
   segment " $base " white 00f
-  segment " $dir " black white 
-  segment prompt_git
+  segment " $dir " black white
   segment_close
 end
